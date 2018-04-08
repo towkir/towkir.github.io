489489b30eec1314a198dd3982f2c83c89379b79
Towkir Ahmed <towkir@Towkirs-MacBook-Pro.local>
2018-04-08 22:35:56 +0600

    added resource sharing platform in projects

---
+++ projects/rsp/README.md
@@ -0,0 +1,33 @@
+# Environment setup
+In order to run this project locally, you will have to setup your local eveironment following these steps.
+Make sure you have nodejs installed and type these commands in your terminal at the project root folder: (if you have gulp installed locally, you can ignore it)
+```sh
+$ npm install gulp --save-dev 
+```
+gulp packages you would need are:
+
+* gulp-less
+* gulp-rename
+* gulp-minify-css
+* gulp-watch
+* gulp-minify
+* gulp-inject-partials
+
+install them by running these commands one after another:
+
+```sh
+$ npm install gulp-less --save-dev
+$ npm install gulp-rename --save-dev
+$ npm install gulp-minify-css --save-dev
+$ npm install gulp-minify-css --save-dev
+$ npm install gulp-watch --save-dev
+$ npm install gulp-minify --save-dev
+$ npm install gulp-inject-partials --save-dev
+```
+now run:
+```sh
+$ gulp
+```
+and you are good to go.
+gulp should start compiling necessary files.
+open up the '/src/index.html' file in your browser to see the result.
---
+++ projects/rsp/gulpfile.js
@@ -0,0 +1,90 @@
+/*
+ to start with gulp
+ npm install --global gulp
+ npm install gulp --save-dev
+ npm install gulp-less --save-dev
+ npm install gulp-rename --save-dev
+ npm install gulp-minify-css --save-dev
+ npm install gulp-watch  --save-dev
+ npm install gulp-minify --save-dev
+ npm install gulp-inject-partials --save-dev 
+ npm install gulp-placeimg --save-dev 
+ npm install gulp-modify-css-urls --save-dev 
+ npm install gulp-image-size --save-dev 
+ * */
+
+//set dir path as you need
+var templatedir = "./";
+//var node_mod_dir = './node_modules/'; //for internal development, or set path as you need
+
+
+var gulp = require('gulp'),// we just install gulp in project dir and try to use other modules from a centralized node modules dir, but if you need you can install in root of your project
+    less = require('gulp-less'),
+    rename = require('gulp-rename'),
+    minifyCss = require('gulp-minify-css'),
+    watch = require('gulp-watch'),
+    minify = require('gulp-minify'),
+    injectPartials = require('gulp-inject-partials');
+
+/* Task to compile less */
+gulp.task('compile-less', function () {
+    var less_tasks = [];
+    var default_less = gulp.src(templatedir + 'src/assets/less/style-default.less')
+        .pipe(less())
+        .pipe(gulp.dest(templatedir + 'src/assets/css/')).on('end', function () {
+            var default_less2 = gulp.src(templatedir + 'src/assets/css/style-default.css')
+                .pipe(minifyCss().on('error', function (err) {
+                    console.log(err);
+                }))
+                .pipe(rename({suffix: '.min'}))
+                .pipe(gulp.dest(templatedir + 'src/assets/css/'));
+            less_tasks.push(default_less2);
+        });
+    return less_tasks;
+});
+
+gulp.task('compress-js', function () {
+    gulp.src(templatedir + 'src/assets/js/theme.js')
+        .pipe(minify({
+            ext: {
+                // src:'-debug.js',
+                min: '.min.js'
+            },
+            exclude: ['tasks'],
+            ignoreFiles: ['.combo.js', '-min.js']
+        }))
+        .pipe(gulp.dest(templatedir + 'src/assets/js'));
+});
+
+/* Task to watch less changes */
+gulp.task('watch-less', ['compile-less'], function () {
+    //watch less file
+    gulp.watch(templatedir + 'src/assets/less/*.less', ['compile-less']);
+    gulp.watch(templatedir + 'src/assets/less/mixins/*.less', ['compile-less']);
+    gulp.watch(templatedir + 'src/assets/less/variation/*.less', ['compile-less']);
+    //watch js file
+    gulp.watch(templatedir + 'src/assets/js/theme.js', ['compress-js']);
+    //compile html and output to src folder
+    gulp.watch(templatedir + 'generator/*.html', ['compile-html']);
+    gulp.watch(templatedir + 'generator/partial/*.html', ['compile-html']);
+});
+
+gulp.task('compile-html', function () {
+    var variation = [
+        "index.html"
+    ];
+    var less_tasks = [];
+    variation.forEach(function (filename) {
+        var temp = gulp.src('./generator/' + filename)
+            .pipe(injectPartials({
+                removeTags: true
+            }))
+            .pipe(gulp.dest('./src'));
+        less_tasks.push(temp);
+    });
+    return less_tasks;
+});
+
+
+/* Task when running `gulp` from terminal */
+gulp.task('default', ['watch-less', 'compress-js', 'compile-html']);
