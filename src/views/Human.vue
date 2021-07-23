b8f280b2299a22e8a4e078e292c17ca1cfb5a37b
Towkir Ahmed <towkir17@gmail.com>
2021-07-23 15:31:59 +0600

    added image and made human page content ready

--- src/views/Human.vue
+++ src/views/Human.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="home">
-    <h1>Towkir Ahmed</h1>
-    <h5>Fronted Engineer | JS Enthusiast | Open Source contributor</h5>
+    <div class="intro">
+      <img src="@/assets/images/towkir.jpg" class="profile-photo" alt="photo-of-towkir">
+      <h1>{{about.name}}</h1>
+      <h5>{{about.bio}}</h5>
+    </div>
   </div>
 </template>
 
 <script>
+import commands from '@/commands.json';
+
 export default {
   name: 'Human',
+  computed: {
+    about() {
+      return {
+        name: commands.whoami.content.split('\n')[0],
+        bio: commands.whoami.content.split('\n')[1],
+      };
+    },
+  },
 };
 </script>
 
@@ -15,5 +28,12 @@ export default {
 .home {
   font-family: 'Avenir', sans-serif;
   padding: 80px;
+  .intro {
+    .profile-photo {
+      max-width: 150px;
+      border-radius: 5px;
+      // border: 2px solid var(--text-color);
+    }
+  }
 }
 </style>
