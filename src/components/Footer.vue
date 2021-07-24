84ed8c5f9368e54e80ddaae5c2a53d430351dafe
Towkir Ahmed <towkir17@gmail.com>
2021-07-24 15:13:31 +0600

    footer component added

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="footer">
+    <p>Developed by the author himself. Icon fonts from <code>fontisto.com</code></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
+
+<style lang="less" scoped>
+.footer {
+  padding: 20px 80px;
+  font-size: 16px;
+  max-width: 1020px;
+  margin: 0 auto;
+  border-top: 1px solid var(--border-color);
+  font-size: 14px;
+}
+</style>
