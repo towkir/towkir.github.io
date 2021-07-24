e5136bb4619ee3e3ec535188d8ec4905e6a9d847
Towkir Ahmed <towkir17@gmail.com>
2021-07-24 15:13:55 +0600

    footer attached to human page

--- src/views/Human.vue
+++ src/views/Human.vue
@@ -1,33 +1,37 @@
 <template>
-  <div class="home">
-    <div class="intro">
-      <img src="@/assets/images/towkir.jpg" class="profile-photo" alt="photo-of-towkir">
-      <div class="details">
-        <h1>{{about.name}}</h1>
-        <h5>{{about.bio}}</h5>
-        <ul class="network-links">
-          <li v-for="(network, n) in networks.content" :key="network.name + n">
-            <a :href="network.link" target="_blank">
-              <i class="fi fi-border ellipse" :class="`${network.iconClass}`"></i>
-            </a>
-          </li>
-        </ul>
+  <div class="wrapper">
+    <div class="home">
+      <div class="intro">
+        <img src="@/assets/images/towkir.jpg" class="profile-photo" alt="photo-of-towkir">
+        <div class="details">
+          <h1>{{about.name}}</h1>
+          <h5>{{about.bio}}</h5>
+          <ul class="network-links">
+            <li v-for="(network, n) in networks.content" :key="network.name + n">
+              <a :href="network.link" target="_blank">
+                <i class="fi fi-border ellipse" :class="`${network.iconClass}`"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
+      <InfoBlock :info="experience" :view="'table'"/>
+      <InfoBlock :info="skills"/>
+      <InfoBlock :info="achievements"/>
+      <InfoBlock :info="contributions"/>
     </div>
-    <InfoBlock :info="experience" :view="'table'"/>
-    <InfoBlock :info="skills"/>
-    <InfoBlock :info="achievements"/>
-    <InfoBlock :info="contributions"/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import InfoBlock from '@/components/InfoBlock.vue';
+import Footer from '@/components/Footer.vue';
 import commands from '@/commands.json';
 
 export default {
   name: 'Human',
-  components: { InfoBlock },
+  components: { InfoBlock, Footer },
   computed: {
     about() {
       return {
@@ -55,37 +59,39 @@ export default {
 </script>
 
 <style lang="less">
-.home {
+.wrapper {
   font-family: 'Avenir', sans-serif;
-  padding: 80px;
-  font-size: 16px;
-  max-width: 1020px;
-  margin: 0 auto;
-  .intro {
-    margin-bottom: 30px;
-    display: flex;
-    align-items: flex-start;
-    .profile-photo {
-      max-width: 150px;
-      border-radius: 5px;
-    }
-    .details {
-      padding: 20px;
-      .network-links {
-        list-style: none;
-        margin-top: 10px;
-        li {
-          display: inline-block;
-          &:not(&:last-child) {
-            margin-right: 5px
-          }
-          a {
+  .home {
+    padding: 80px;
+    font-size: 16px;
+    max-width: 1020px;
+    margin: 0 auto;
+    .intro {
+      margin-bottom: 30px;
+      display: flex;
+      align-items: flex-start;
+      .profile-photo {
+        max-width: 150px;
+        border-radius: 5px;
+      }
+      .details {
+        padding: 20px;
+        .network-links {
+          list-style: none;
+          margin-top: 10px;
+          li {
             display: inline-block;
-            color: var(--text-color);
-            font-size: 14px;
-            i {
-              border-color: var(--text-color);
-              border-width: 1px;
+            &:not(&:last-child) {
+              margin-right: 5px
+            }
+            a {
+              display: inline-block;
+              color: var(--text-color);
+              font-size: 14px;
+              i {
+                border-color: var(--text-color);
+                border-width: 1px;
+              }
             }
           }
         }
